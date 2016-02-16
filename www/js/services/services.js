ae5ae5cd1e8cd932d54350fd7de96dd2180b4325
UdeeshaDissanayake <shehanudeesha@gmail.com>
2016-02-16 14:09:08 +0530

    change UI

--- www/js/services/services.js
+++ www/js/services/services.js
@@ -13,21 +13,24 @@ angular.module('homepi.services', ['ngResource'])
 
 
        // client =mqttws31.Client(orgId+".messaging.internetofthings.ibmcloud.com", 1883, clientId);
+        /*creating mqtt client for creating connection*/
        client=mqtt.createClient(1883,orgId+".messaging.internetofthings.ibmcloud.com",  {
            "clientId" : clientId,
            "keepalive" : 30,
            "username" : "use-token-auth",
            "password" : password
        });
-
+        //if disconnect the connection
         client.on('disconnect', function () {
             isConnected=false;
             console.log('DisConnected');
         });
+        //if connected
         client.on('connect', function () {
             isConnected=true;
             console.log('Connected');
         });
+        //if there are errors
         client.on('error', function(err) {
             console.log('error!', err);
             client.stream.end();
@@ -37,16 +40,18 @@ angular.module('homepi.services', ['ngResource'])
             service.callback(topic,message);
         });
     }
-
+   //publishing mqtt messages
     service.publish = function(topic, payload) {
         client.publish(topic,payload, {retain: true});
         console.log('publish-Event sent '+ payload + ' with topic: ' + topic + ' ' + client);
     }
+    //disconnecting
     service.disconnect=function(){
         client.end(true);
     }
-    service.isConnected = function() {
 
+    //check wheter connect or not
+    service.isConnected = function() {
         console.log(isConnected);
         return isConnected;
     }
